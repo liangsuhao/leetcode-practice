5891b42c5fb08aca4055ae87fc8570afa43d3fdc
liangsuhao <2450381315@qq.com>
2020-11-12 00:03:18 +0800

    p88

---
+++ p88.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ * 题目：
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+ 
+
+说明：
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ 
+
+示例：
+
+输入：
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+输出：[1,2,2,3,5,6]
+
+ */
+var merge = function(nums1, m, nums2, n) {
+    nums1 = nums1.slice(0,m);
+    for(var i=0,j=0;i<n;i++){
+        for(;j<m+n;j++){
+            if((nums2[i]>=nums1[j] && nums2[i]<nums1[j])||(nums2[i]>=nums1[j] && !nums1[j])){
+                nums1.splice(j,0,nums2[i]);
+                break;
+            }
+        }
+    }
+    return nums1;
+};
