53c5529bbced0a4026f927c6834dd164fa56135e
liangsuhao <2450381315@qq.com>
2021-03-04 23:18:07 +0800

    链表相加

--- listNode/p445.js
+++ listNode/p445.js
@@ -28,5 +28,48 @@
 输出：7 -> 8 -> 0 -> 7
  */
 var addTwoNumbers = function(l1, l2) {
+    const stack1 = [];
+  const stack2 = [];
+  const stack = [];
 
+  let cur1 = l1;
+  let cur2 = l2;
+  let curried = 0;
+
+  while (cur1) {
+    stack1.push(cur1.val);
+    cur1 = cur1.next;
+  }
+
+  while (cur2) {
+    stack2.push(cur2.val);
+    cur2 = cur2.next;
+  }
+
+  let a = null;
+  let b = null;
+
+  while (stack1.length > 0 || stack2.length > 0) {
+    a = Number(stack1.pop()) || 0;
+    b = Number(stack2.pop()) || 0;
+
+    stack.push((a + b + curried) % 10);
+
+    curried = a + b + curried >= 10 ? 1 : 0;
+  }
+
+  if (curried === 1) {
+    stack.push(1);
+  }
+
+  const dummy = new ListNode();
+
+  let current = dummy;
+
+  while (stack.length > 0) {
+    current.next = new ListNode(stack.pop());
+    current = current.next;
+  }
+
+  return dummy.next;
 };
