9aa7e69a72b0dfb5a3dda07f20fa713a06c0a17e
liangsuhao <2450381315@qq.com>
2022-02-09 23:42:49 +0800

    397 420

---
+++ greedy/p402.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ * 题目：
+ * 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+
+示例 1 ：
+
+输入：num = "1432219", k = 3
+输出："1219"
+解释：移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219 。
+示例 2 ：
+
+输入：num = "10200", k = 1
+输出："200"
+解释：移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+ */
+ var removeKdigits = function(num, k) {
+    for(let i=0;i<num.length-1 && k>0;i++) {
+        if(num[i] > num[i+1]) {
+            num = num.substring(0,i)+num.substr(i+1,num.length);
+            i=i-2;
+            k--;
+        }
+    }
+    if(k > 0){
+        num = num.substring(0,num.length-k);
+    }
+    while(num[0] === '0') {
+        num = num.substring(1);
+    }
+    if(num === "") {
+        num = '0';
+    }
+    return num;
+};
